f2869119f6422c85164c223d8e8f948a23d46ebf
George Wilson <georgejameswilson@googlemail.com>
2020-04-14 12:09:37 +0100

    Generate a config for Joomla 1.5

---
+++ translation-cli/libraries/version-config-15.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Version configuration file for version 1.5.x
+ */
+class VersionConfig
+{
+	// Name of XML file on update site
+	var $xmlFile = 'translationlist_15.xml';
+
+	// Name of details folder on cron server
+	var $detailsFolder = 'details15';
+
+	// Name (file path) of project on Joomlacode.org where language files can be downloaded
+	var $project = 'jtranslation';
+
+	// Name of folder on update site where detail XML files are found for each language
+	var $updateFolder = 'http://update.joomla.org/language/details15/';
+}
